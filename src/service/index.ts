d3ae66b3dbffef6d83668864058b28c63769132b
葛昭龙 <10737548+gzllovelsj@user.noreply.gitee.com>
2023-06-05 13:19:31 +0000

    !3 登录功能 * fix(other): 解决代码冲突问题 * feat(views): 登录功能页面 * feat(router): 路由跳转 * feat(router): 更改路由配置 * docs(other): 更改提交的分支

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,11 +1,29 @@
+/* eslint-disable import/no-named-as-default-member */
 /* eslint-disable import/prefer-default-export */
+// eslint-disable-next-line import/no-named-as-default
 import request from '@/service/request'
-// import { axr } from 'axios'
-
-interface ApiResponse {
-  // 此处的类型定义需要根据实际接口返回的数据结构进行编写
+import { AxiosPromise } from 'axios'
+// /**
+//  * 验证码接口
+//  */
+type ApiResponse = {
   code: number
-  message: string
-  data: any
+  msg: string
+
+  data: { svg: string; no: string }
 }
-export const getLogin = () => request.get<ApiResponse>('/api/admin/verifycode')
+
+// export const getLogin = () => request.get<AxiosPromise<ApiResponse>>('/api/admin/verifycode')
+export const getLogin = async () => {
+  const res = <AxiosPromise<ApiResponse>>request.get('/admin/verifycode')
+  return (await res).data.data
+}
+interface LoginParams {
+  adminName: string
+  adminPwd: string
+  verifyCode: string
+  no: string
+}
+type ILoginResponseData = { code: number; msg: string }
+export const LoginAdmin = (pararms: LoginParams): AxiosPromise<ILoginResponseData> =>
+  request.post('admin/login', pararms)
